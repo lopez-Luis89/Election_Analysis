0c32c616a016e32fd791ab0ae25131b0d8a98623
lopez-Luis89 <luislopez@otaymastertruckcenter.com>
2022-03-07 10:04:56 -0800

    Adding The Challenge

--- Pypoll.py
+++ Pypoll.py
@@ -16,15 +16,20 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 file_to_save = os.path.join("analysis", "election_results.txt")
 total_votes = 0
 candidate_options = []
+candidate_county_names =[]
 candidates_vote = {}
+candidate_county_votes = {}
 winning_candidate = ""
+winning_county = ""
 winning_count = 0
+winning_county_count = 0
 winning_percentage = 0
+winning_county_percentage = 0
 
 #Writing text to another file using the open function
 with  open(file_to_save, "w") as txt_file:
     txt_file.write("Counties in the Election")
-    txt_file.write("\n///////////////////////////////\n")
+    txt_file.write("///////////////////////////////\n")
     txt_file.write("Arapahoe\nDenver\nJefferson")
 
 
@@ -43,6 +48,7 @@ with open(file_to_load) as election_data:
         
        # Print the candidate name from each row.
         candidate_name = row[2]
+    
         
         if candidate_name not in candidate_options:
              # Add the candidate name to the candidate list.
@@ -51,23 +57,71 @@ with open(file_to_load) as election_data:
             candidates_vote[candidate_name] = 0
     
         candidates_vote[candidate_name] += 1
+        
+
+
+        #-----------Analyzing by county-------------
+        candidate_county = row[1] # Created a list and dictionay for the respective variables
+                                  #And kep count for each county and printed results of votes per county
+
+        if candidate_county not in candidate_county_names:
+            candidate_county_names.append(candidate_county)
+            candidate_county_votes[candidate_county] = 0
+        candidate_county_votes[candidate_county] += 1
+    #print(candidate_county_votes)  
+        #----------Analyzing by county--------------
+        #----------Determining Winning County -----
+    for candidate_county in candidate_county_votes:
+            county_votes = candidate_county_votes[candidate_county]
+            county_percentage = round(float(county_votes) / float(total_votes) * 100)
+
+            if (county_votes > winning_county_count) and (county_percentage > winning_county_percentage):
+                winning_county_count = county_votes
+                winning_county = candidate_county
+                winning_county_percentage = county_percentage
+            county_result = (f"{candidate_county}: {county_percentage:.1f}% ({county_votes:,})")
+            print(county_result)#****Prints out everything****
+         # ----------Determining Winning county------  
+
 with open(file_to_save,"w") as txt_file:      
     election_results = (
         f"\n Election Results\n"
         f"-------------------\n"
         f"Total Votes: {total_votes:,}\n"
-        f"-------------------\n")
+        f"--------{county_result}-----------\n" #*****Only prints out last city*************
+        )
     print(election_results,end ="")  
     
-    txt_file.write(election_results)      
+    txt_file.write(election_results)  
+
+    candidate_county = row[1] # Created a list and dictionay for the respective variables
+                                  #And kep count for each county and printed results of votes per county
+
+            if candidate_county not in candidate_county_names:
+                candidate_county_names.append(candidate_county)
+                candidate_county_votes[candidate_county] = 0
+            candidate_county_votes[candidate_county] += 1
+    #print(candidate_county_votes)  
+        #----------Analyzing by county--------------
+        #----------Determining Winning County -----
+    for candidate_county in candidate_county_votes:
+            county_votes = candidate_county_votes[candidate_county]
+            county_percentage = round(float(county_votes) / float(total_votes) * 100)
+
+            if (county_votes > winning_county_count) and (county_percentage > winning_county_percentage):
+                winning_county_count = county_votes
+                winning_county = candidate_county
+                winning_county_percentage = county_percentage
+            county_result = (f"{candidate_county}: {county_percentage:.1f}% ({county_votes:,})")
+            print(county_result)#****Prints out everything****
+         # ----------Determining Winning county------  
+
 
     for candidate_name in candidates_vote:
                     
             votes = candidates_vote[candidate_name]
             vote_percentage = round(float(votes) / float(total_votes) * 100)
             
-            
-
             if (votes > winning_count) and (vote_percentage > winning_percentage ):
                 winning_count = votes
                 winning_percentage = vote_percentage
